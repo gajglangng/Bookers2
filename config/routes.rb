003dd4a9f37b44ef85551bbed400899201f8170f
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-08-20 01:04:38 +0000

    user-edit追加/application controller追記

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
   resources :books, only: [:new, :index, :show, :update]
-  resources :user, only: [:index, :show]
+  resources :user, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
