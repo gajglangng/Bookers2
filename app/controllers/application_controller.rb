72bad952cf64b392619d5703c99d0712aa0aa585
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-08-21 02:41:40 +0000

    code整理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,23 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    about_path
+  def after_sign_in_path_for(resource) #ログイン後マイページ
+    user_path(resource)
   end
 
-  def after_sign_out_path_for(resource)
-    about_path
+  def after_sign_out_path_for(resource) #ログアウト後トップページ
+    root_path(resource)
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :postal_code, :prefecture_code, :city, :street])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
+  
+  def redirect_login
+    redirect_to new_user_session_path unless user_signed_in?
   end
   
 end
