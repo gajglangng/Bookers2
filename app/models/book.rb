352ae555fe1be926eea81f79ab9efd333fdd42a7
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-09-01 10:25:29 +0000

    1:N

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+  
+  belongs_to :user #1:N
+  
   validates :title, presence: true
   validates :body, presence: true
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true
-  validates :introduction, presence: true
-         
+  
+  has_one_attached :profile_image           
   has_many :books, dependent: :destroy
   
 end
