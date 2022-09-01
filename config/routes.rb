84f48cb44ed849d74571c0982d4822d432a73d97
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-09-01 10:20:36 +0000

    route修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#top"
-  get "/home/about" => "homes#about"
+  root to: 'homes#top'
+  get '/home/about' => 'homes#about'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
   end  
   resources :users, only: [:index, :show, :edit, :update] do
