f866b010c2a22b9827a527ec931bf7457aa7d1b3
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2022-09-04 04:54:11 +0000

    introduction反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,13 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :books, dependent: :destroy
-  attachment :profile_image
+  has_one_attached :profile_image
+  
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
 end
